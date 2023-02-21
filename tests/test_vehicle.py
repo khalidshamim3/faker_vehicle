dff11106521afbb47a6cba1a601d6a44dd4d0364
Som.Mohapatra <Som.Mohapatra@tgs.com>
2023-02-21 12:49:24 +0530

    testing tag v.1.3

--- tests/test_vehicle.py
+++ tests/test_vehicle.py
@@ -8,22 +8,12 @@ def test_vehicles(fake, vehicles):
     
 
 def test_vehicles_4(fake, vehicles):
+    ##testing tag v.1.3
+    tag = 'v.1.3'
     assert len(vehicles) > 4
     v = vehicles[0]
     assert 'Make' in v.keys()
     assert 'Model' in v.keys()    
-
-
-def test_vehicles_3(fake, vehicles):
-    assert len(vehicles) > 3
-
-def test_vehicles_2(fake, vehicles):
-    assert len(vehicles) > 2
-
-    v = vehicles[0]
-    assert 'Make' in v.keys()
-    assert 'Model' in v.keys()
-
 
 
 def test_make(fake, makes):
